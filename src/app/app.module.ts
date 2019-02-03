9bd1dc487a43fe412ea386608e5b6099f115d555
pamplemousseMR <romain.manciaux@gmail.com>
2019-02-03 16:08:45 +0100

    feat(routing): add rout for home and research

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { ResearchComponent } from './research/research.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    ResearchComponent
   ],
   imports: [
     BrowserModule,
